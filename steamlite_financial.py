919024fa983209c8fc5639706752f6edbf59cd55
Varshaa <varshaa524@wec.edu.in>
2026-04-08 00:33:04 +0530

    Removed colab magic commands

--- steamlite_financial.py
+++ steamlite_financial.py
@@ -8,7 +8,6 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-%%writefile app.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
